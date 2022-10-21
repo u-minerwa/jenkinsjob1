249f7442ea3d0af608044e6d9e875459eb5f1890
eternity <92870607+u-minerwa@users.noreply.github.com>
2022-10-21 23:20:19 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -36,6 +36,14 @@ pipeline {
 		  }
 	  }
 	    
+	  post {
+        always{
+           emailext attachLog: true, body: '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
+            Check console output at $BUILD_URL to view the results.''', subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!',
+                to: 'cahek6599@gmail.com'
+            }
+        }  
+	    
     }
 }
 
