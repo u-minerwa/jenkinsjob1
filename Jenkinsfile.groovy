71f66ea18148f27ea08710048a39f5b0f33474c6
eternity <92870607+u-minerwa@users.noreply.github.com>
2022-10-21 23:00:19 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,6 +13,28 @@ pipeline {
             }
         }
 	    
+	 stage('Archive'){
+                steps{
+			dir('C:\\'){
+				echo "Current build: ${BUILD_NUMBER}"
+				zip zipFile: "${BUILD_NUMBER}.zip", archive:false, dir: 'ProgramData\Jenkins.jenkins\workspace\firstjob\Calc\bin\Debug\netcoreapp3.1'
+				archiveArtifacts artifacts: "${BUILD_NUMBER}.zip"
+			}
+		  }
+    	}
+	    stage('Deploy'){
+		  steps{
+			  dir('C:\\'){
+				  script{
+					  try
+					  {
+						  bat("md C:\\Deploy\\")
+					  }catch(Exception e){}
+				  }
+				  unzip zipFile: "${BUILD_NUMBER}.zip", dir: 'C:\\Deploy'
+			  }
+		  }
+	  }
 	    
     }
 }
